7a12ae6d268e78cef59476c65e8d0ff70f0a5c60
laserjobs <github@flemingink.com>
2025-09-21 15:35:59 -1000

    Create utils.py

---
+++ src/prometheus_core/utils.py
@@ -0,0 +1,23 @@
+# src/prometheus_core/utils.py
+import torch
+
+# Simple char-level tokenizer for demonstration
+class CharTokenizer:
+    def __init__(self):
+        self.chars = sorted(list(set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,;:!?'\"-()[]{}\n")))
+        self.stoi = {ch: i for i, ch in enumerate(self.chars)}
+        self.itos = {i: ch for i, ch in enumerate(self.chars)}
+        self.vocab_size = len(self.chars)
+
+    def encode(self, s):
+        return [self.stoi[c] for c in s if c in self.stoi]
+
+    def decode(self, l):
+        return "".join([self.itos[i] for i in l])
+
+# Simple dummy data generator for training
+def get_batch(data, block_size, batch_size, device):
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i : i + block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    return x.to(device), y.to(device)
