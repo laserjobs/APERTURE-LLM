bc956f935264096c87753b2f3efab571cc5f4de1
laserjobs <github@flemingink.com>
2025-09-27 15:06:55 -1000

    Update infer_model.py

--- src/scripts/infer_model.py
+++ src/scripts/infer_model.py
@@ -1,16 +1,15 @@
 import sys
 import os
 import warnings
+import torch  # Moved up
+import yaml  # Moved up
+from types import SimpleNamespace  # Moved up
 
 # Suppress FutureWarning from torch.load
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 # Add src/aperture_core to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-import torch
-import yaml
-from types import SimpleNamespace
 
 from aperture_core.model import APERTURE_LLM
 from aperture_core.utils import CharTokenizer, set_seed
